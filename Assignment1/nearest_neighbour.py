b3c46b11b3797e09ce7bdb2faa10cf20064a685c
taliBerman5 <taliberman5@gmail.com>
2021-11-22 11:41:51 +0200

    update

--- Assignment1/nearest_neighbour.py
+++ Assignment1/nearest_neighbour.py
@@ -112,7 +112,6 @@ def verySimpleTest():
 def testRepeaterOverTrainSize():
     k = 1
     data = np.load('mnist_all.npz')
-    testLen = 4085
     train1 = data['train1']
     train3 = data['train3']
     train4 = data['train4']
@@ -123,6 +122,7 @@ def testRepeaterOverTrainSize():
     test4 = data['test4']
     test6 = data['test6']
 
+    testLen = max(len(test1), len(test3), len(test4), len(test6))
     testSize = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
     x_test, y_test = gensmallm([test1, test3, test4, test6], [1, 3, 4, 6], testLen)
 
@@ -153,8 +153,8 @@ def testRepeaterOverTrainSize():
 
 def corrupt(y):
     size = len(y)
-    indexes = [random.randint(0, size-1) for i in range(int(size/5))]
-    for i in range(int(size/5)):
+    indexes = [random.randint(0, size - 1) for i in range(int(size / 5))]
+    for i in range(int(size / 5)):
         options = [1, 3, 4, 6]
         options.remove(y[i])
         y[i] = random.choice(options)
@@ -162,7 +162,6 @@ def corrupt(y):
 
 def testRepeaterOverK(corrupted):
     data = np.load('mnist_all.npz')
-    testLen = 4085
     train1 = data['train1']
     train3 = data['train3']
     train4 = data['train4']
@@ -173,7 +172,7 @@ def testRepeaterOverK(corrupted):
     test4 = data['test4']
     test6 = data['test6']
 
-    #testLen = max(len(test1), len(test3), len(test4), len(test6))
+    testLen = max(len(test1), len(test3), len(test4), len(test6))
     x_test, y_test = gensmallm([test1, test3, test4, test6], [1, 3, 4, 6], testLen)
 
     meansError = np.array([])
@@ -196,31 +195,30 @@ def testRepeaterOverK(corrupted):
 
 if __name__ == '__main__':
     # before submitting, make sure that the function simple_test runs without errors
-    # meansErrorTrain, testSizeTrain, minError, maxError = testRepeaterOverTrainSize()
-    # plt.plot(testSizeTrain, meansErrorTrain, color="blue")
-    # plt.plot(testSizeTrain, minError, color="green")
-    # plt.plot(testSizeTrain, maxError, color="red")
-    # plt.errorbar(testSizeTrain,
-    #              meansErrorTrain,
-    #              [meansErrorTrain - minError, maxError - meansErrorTrain],
-    #              fmt='ok', lw=1,
-    #              ecolor='tomato')
-    # plt.legend(["Average test error", "Minimum Error", "Maximum Error"])
-    # plt.title("Error over train size")
-    # plt.xlabel("Train size")
-    # plt.ylabel("Average test error")
-    # plt.show()
-    # plt.close()
-    #
-    # meansErrorK, testSizeK = testRepeaterOverK(False)
-    # plt.plot(testSizeK, meansErrorK, color="blue")
-    # plt.legend(["Average test error"])
-    # plt.title("Error over K")
-    # plt.xlabel("K")
-    # plt.ylabel("Average test error")
-    # plt.show()
-    # plt.close()
-    #
+    meansErrorTrain, testSizeTrain, minError, maxError = testRepeaterOverTrainSize()
+    plt.plot(testSizeTrain, meansErrorTrain, color="blue")
+    plt.plot(testSizeTrain, minError, color="green")
+    plt.plot(testSizeTrain, maxError, color="red")
+    plt.errorbar(testSizeTrain,
+                 meansErrorTrain,
+                 [meansErrorTrain - minError, maxError - meansErrorTrain],
+                 fmt='ok', lw=1,
+                 ecolor='tomato')
+    plt.legend(["Average test error", "Minimum Error", "Maximum Error"])
+    plt.title("Error over train size")
+    plt.xlabel("Train size")
+    plt.ylabel("Average test error")
+    plt.show()
+    plt.close()
+
+    meansErrorK, testSizeK = testRepeaterOverK(False)
+    plt.plot(testSizeK, meansErrorK, color="blue")
+    plt.legend(["Average test error"])
+    plt.title("Error over K")
+    plt.xlabel("K")
+    plt.ylabel("Average test error")
+    plt.show()
+    plt.close()
 
     meansErrorK, testSizeK = testRepeaterOverK(True)
     plt.plot(testSizeK, meansErrorK, color="blue")
@@ -230,7 +228,3 @@ if __name__ == '__main__':
     plt.ylabel("Average test error")
     plt.show()
     plt.close()
-
-
-
-
