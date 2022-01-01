04b431a5ced5c9dc5f9678ad23217db4473c996c
taliBerman5 <taliberman5@gmail.com>
2022-01-01 14:01:37 +0200

    fixed task2a and added task2 c d

--- Assignment3/task2.py
+++ Assignment3/task2.py
@@ -5,15 +5,20 @@ import matplotlib.pyplot as plt
 
 def parse_data(num1, num2, amount_train):
     data = np.load('mnist_all.npz')
-    train0 = np.where(data[f'train{num1}'] > 128, 1, 0)
-    train1 = np.where(data[f'train{num2}'] > 128, 1, 0)
 
-    test0 = np.where(data[f'test{num1}'] > 128, 1, 0)
-    test1 = np.where(data[f'test{num2}'] > 128, 1, 0)
+    train0 = data[f'train{num1}']
+    train1 = data[f'train{num2}']
 
-    testLen = max(len(test0), len(test1))
-    x_test, y_test = gensmallm([test0, test1], [-1, 1], testLen)
+    test0 = data[f'test{num1}']
+    test1 = data[f'test{num2}']
+
+    testLen = len(test0) + len(test1)
     x_train, y_train = gensmallm([train0, train1], [-1, 1], amount_train)
+    x_test, y_test = gensmallm([test0, test1], [-1, 1], testLen)
+
+    threshold = 128
+    x_train = np.where(x_train > threshold, 1, 0)
+    x_test = np.where(x_test > threshold, 1, 0)
     return x_train, y_train, x_test, y_test
 
 
@@ -25,7 +30,7 @@ def naive_Bayes(num1, num2, train_amounts):
         x_train, y_train, x_test, y_test = parse_data(num1, num2, amount)
 
         y_predict = bayespredict(*bayeslearn(x_train, y_train), x_test)
-        error.append(np.mean(y_test != y_predict))
+        error.append(np.mean(y_test.reshape(len(y_test), 1) != y_predict))
     return error
 
 def task2a():
@@ -40,8 +45,37 @@ def task2a():
     plt.ylabel("error")
     plt.show()
 
+def task2cd(num1, num2):
+    x_train, y_train, x_test, y_test = parse_data(num1, num2, 10000)
+    allpos, ppos, pneg = bayeslearn(x_train, y_train)
+
+    heatMap('ppos', ppos)
+    heatMap('pneg', pneg)
+
+    y_predict = bayespredict(allpos, ppos, pneg, x_test)
+    y_predict_075 = bayespredict(0.75, ppos, pneg, x_test)
+
+    l = y_predict.shape[0]
+    changed_to_minus = np.asarray([1 if y_predict[i] == 1 and y_predict_075[i] == -1 else 0 for i in range(l)])
+    changed_from_minus = np.asarray([1 if y_predict[i] == -1 and y_predict_075[i] == 1 else 0 for i in range(l)])
+
+    print(f'The precent of test set example their labels changed from 1 to -1 is {np.mean(changed_to_minus)}')
+    print(f'The precent of test set example their labels changed from -1 to 1 is {np.mean(changed_from_minus)}')
+
+
+
+
+def heatMap(title, img):
+    plt.imshow(img.reshape(28, 28), cmap='hot')
+    plt.title(f'{title} for classification differentiating 0 and 1')
+    plt.show()
+
+
+
+
 
 task2a()
-
+task2cd(0, 1)
+task2cd(3, 5)
 
 
